0929611cd7f596f87d65b92fd767a31dc5e7f76f
unknown <Amit Ranjan Pradhan>
2021-06-22 10:22:11 +0530

    Added: UC2-New Contact By Using Console

--- src/main/java/com/addressbook/AddressBookMain.java
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -2,14 +2,42 @@ package com.addressbook;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookMain {
     /**
      * Added welcome message
+     * Taking console input.
      * @param args
      */
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter your first name:\n");
+        String firstname=scan.next();
+        System.out.println("Enter your last name:\n");
+        String lastname=scan.next();
+        System.out.println("Enter your Address:\n");
+        String address=scan.next();
+        System.out.println("Enter your City:\n");
+        String city=scan.next();
+        System.out.println("Enter your State:\n");
+        String state=scan.next();
+        System.out.println("Enter your Zip:\n");
+        String zip=scan.next();
+        System.out.println("Enter your Phone Number:\n");
+        String phoneNumber=scan.next();
+        System.out.println("Enter your Email:\n");
+        String email=scan.next();
+        Addressbook contactList=new Addressbook(firstname,lastname,address,city,state,zip,phoneNumber,email);
+    }
+    /**
+     * this method will return the given input as a string.
+     * @return next input.
+     */
+    public String getInput() {
+        Scanner scan=new Scanner(System.in);
+        return scan.next();
     }
     /**
      * added this method to check the test case.
--- src/test/java/com/addressbook/AddressBookTest.java
+++ src/test/java/com/addressbook/AddressBookTest.java
@@ -3,6 +3,9 @@ package com.addressbook;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class AddressBookTest {
     /**
      * this will check the contact is added or not.
@@ -13,5 +16,36 @@ public class AddressBookTest {
         Addressbook addressbook=new Addressbook("AmitRanjan", "Pradhan", "Narsinghpur", "Cuttack", "Odisha", "754032", "6371912511", "amitranjanpradhan456@gmail.com");
         Boolean result=contact.AddContact(addressbook);
         Assertions.assertTrue(result);
+    }
+    /**
+     * this method will check the user inputs.
+     */
+    @Test
+    public void Given_ConsoleInput_Is_Added_Should_return_value(){
+        AddressBookMain contact=new AddressBookMain();
+        String firstname = "santosh";
+        InputStream in = new ByteArrayInputStream(firstname.getBytes());
+        System.setIn(in);
+        Assertions.assertEquals("santosh",contact.getInput());
+        String lastname = "Panda";
+        InputStream lastName = new ByteArrayInputStream(lastname.getBytes());
+        System.setIn(lastName);
+        Assertions.assertEquals("Panda",contact.getInput());
+        String address = "Bhubaneswar";
+        InputStream aDdress = new ByteArrayInputStream(address.getBytes());
+        System.setIn(aDdress);
+        Assertions.assertEquals("Bhubaneswar",contact.getInput());
+        String city = "bbsr";
+        InputStream cIty = new ByteArrayInputStream(city.getBytes());
+        System.setIn(cIty);
+        Assertions.assertEquals("bbsr",contact.getInput());
+        String state = "odisha";
+        InputStream sTate = new ByteArrayInputStream(state.getBytes());
+        System.setIn(sTate);
+        Assertions.assertEquals("odisha",contact.getInput());
+        String zip = "751011";
+        InputStream zIp = new ByteArrayInputStream(zip.getBytes());
+        System.setIn(zIp);
+        Assertions.assertEquals("751011",contact.getInput());
     }
 }
