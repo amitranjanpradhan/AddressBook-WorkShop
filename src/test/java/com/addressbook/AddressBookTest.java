91386e411a4cd51f88ab97670d690077c9fa3df2
unknown <Amit Ranjan Pradhan>
2021-06-22 11:03:57 +0530

    Added: UC2-New Contact By Using Console

--- src/test/java/com/addressbook/AddressBookTest.java
+++ src/test/java/com/addressbook/AddressBookTest.java
@@ -13,7 +13,7 @@ public class AddressBookTest {
     @Test
     public void Given_variablesAre_present_should_return_true(){
         AddressBookMain contact=new AddressBookMain();
-        Addressbook addressbook=new Addressbook("AmitRanjan", "Pradhan", "Narsinghpur", "Cuttack", "Odisha", "754032", "6371912511", "amitranjanpradhan456@gmail.com");
+        Addressbook addressbook=new Addressbook("Amitranjan", "Pradhan", "Narsinghpur", "Cuttack", "Odisha", "754032", "6371912511", "amitranjanpradhan456@gmail.com");
         Boolean result=contact.AddContact(addressbook);
         Assertions.assertTrue(result);
     }
@@ -23,29 +23,37 @@ public class AddressBookTest {
     @Test
     public void Given_ConsoleInput_Is_Added_Should_return_value(){
         AddressBookMain contact=new AddressBookMain();
-        String firstname = "santosh";
+        String firstname = "Amitranjan";
         InputStream in = new ByteArrayInputStream(firstname.getBytes());
         System.setIn(in);
-        Assertions.assertEquals("santosh",contact.getInput());
-        String lastname = "Panda";
+        Assertions.assertEquals("Amitranjan",contact.getInput());
+        String lastname = "Pradhan";
         InputStream lastName = new ByteArrayInputStream(lastname.getBytes());
         System.setIn(lastName);
-        Assertions.assertEquals("Panda",contact.getInput());
-        String address = "Bhubaneswar";
+        Assertions.assertEquals("Pradhan",contact.getInput());
+        String address = "Narsinghpur";
         InputStream aDdress = new ByteArrayInputStream(address.getBytes());
         System.setIn(aDdress);
-        Assertions.assertEquals("Bhubaneswar",contact.getInput());
-        String city = "bbsr";
+        Assertions.assertEquals("Narsinghpur",contact.getInput());
+        String city = "Cuttack";
         InputStream cIty = new ByteArrayInputStream(city.getBytes());
         System.setIn(cIty);
-        Assertions.assertEquals("bbsr",contact.getInput());
+        Assertions.assertEquals("Cuttack",contact.getInput());
         String state = "odisha";
         InputStream sTate = new ByteArrayInputStream(state.getBytes());
         System.setIn(sTate);
         Assertions.assertEquals("odisha",contact.getInput());
-        String zip = "751011";
+        String zip = "754032";
         InputStream zIp = new ByteArrayInputStream(zip.getBytes());
         System.setIn(zIp);
-        Assertions.assertEquals("751011",contact.getInput());
+        Assertions.assertEquals("754032",contact.getInput());
+        String phoneNumber = "6371912511";
+        InputStream pHoneNumber = new ByteArrayInputStream(zip.getBytes());
+        System.setIn(pHoneNumber);
+        Assertions.assertEquals("6371912511",contact.getInput());
+        String email = "amitranjanpradhan456@gmail.com";
+        InputStream eMail = new ByteArrayInputStream(zip.getBytes());
+        System.setIn(eMail);
+        Assertions.assertEquals("amitranjanpradhan456@gmail.com",contact.getInput());
     }
 }
